0e835a83251d91910d87d47a7fe831d90d7f6728
Jacob Phillips <phillips.jakecolby@gmail.com>
2019-06-19 15:31:00 -0400

    finished

--- data/friends.js
+++ data/friends.js
@@ -2,20 +2,26 @@ var friends = [
     {
         name:"John",
         age:"32",
-        photo: 'linkphotogoeshere',
-        scores: [1,1,1,1,1]
+        photo: 'https://www.unilad.co.uk/wp-content/uploads/2015/12/person-of-the-year-2.jpg',
+        scores: [4,1,2,1,2]
     },
     {
         name:"Bob",
         age:"35",
-        photo: 'linkphotogoeshere',
-        scores: [1,1,1,1,1]
+        photo: 'https://www.unilad.co.uk/wp-content/uploads/2015/12/person-of-the-year-2.jpg',
+        scores: [3,3,1,4,2]
     },
     {
         name:"Paul",
         age:"31",
-        photo: 'linkphotogoeshere',
-        scores: [1,1,1,1,1]
+        photo: 'https://www.unilad.co.uk/wp-content/uploads/2015/12/person-of-the-year-2.jpg',
+        scores: [1,2,4,1,3]
+    },
+    {
+        name:"Rachelle",
+        age:"29",
+        photo: 'https://www.unilad.co.uk/wp-content/uploads/2015/12/person-of-the-year-2.jpg',
+        scores: [3,2,2,1,3]
     }
 ]
 module.exports = friends
